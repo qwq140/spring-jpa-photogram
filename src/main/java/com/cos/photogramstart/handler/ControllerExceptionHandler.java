5e9acc02411be1354a31f6707232b402ac48081e
unknown <qwq140@gmail.com>
2021-07-20 17:18:11 +0900

    8. 구독하기 - 예외처리하기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cos.photogramstart.handler.ex.CustomApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
@@ -27,5 +28,10 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(CustomValidationApiException.class) // RuntimeException이 발동하는 모든 익셉션을 가로챈다.
 	public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationApiException e) {
 		return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),e.getErrorMap()),HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(CustomApiException.class) // RuntimeException이 발동하는 모든 익셉션을 가로챈다.
+	public ResponseEntity<CMRespDto<?>> apiException(CustomApiException e) {
+		return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),null),HttpStatus.BAD_REQUEST);
 	}
 }
--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +16,11 @@ public class SubscribeService {
 	
 	@Transactional
 	public void 구독하기(int fromUserId, int toUserId) {
-		subscribeRepository.mSubscribe(fromUserId, toUserId);
+		try {
+			subscribeRepository.mSubscribe(fromUserId, toUserId);
+		} catch (Exception e) {
+			throw new CustomApiException("이미 구독을 하였습니다.");
+		}
 	}
 	
 	@Transactional
