8bbbfa5fb66440007f259919a4cf432944071b69
unknown <qwq140@gmail.com>
2021-07-28 20:11:24 +0900

    21. 좋아요 구현 - 무한참조 버그 잡기

--- src/main/java/com/cos/photogramstart/domain/likes/Likes.java
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -14,6 +14,7 @@ import javax.persistence.UniqueConstraint;
 
 import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -45,6 +46,7 @@ public class Likes { // N
 	private Image image; // 1
 	
 	// 오류가 터지고 나서 잡아보기
+	@JsonIgnoreProperties({"images"})
 	@JoinColumn(name = "userId")
 	@ManyToOne
 	private User user; // 1
